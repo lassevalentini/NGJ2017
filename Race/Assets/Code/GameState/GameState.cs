cd982dec0a2785415e848c03442da9e40ea43a08
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2017-04-22 15:10:52 +0200

    Wiring to end game

---
+++ Race/Assets/Code/GameState/GameState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState
+{
+
+    public static void EndGame()
+    {
+        Debug.Log("Game end!");
+        //UiManager.Instance
+    }
+}
---
+++ Race/Assets/Code/LavaBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(string.Format("Collision with {0}", col.gameObject));
+        if (col.gameObject.tag == "Player")
+        {
+            GameState.EndGame();
+        }
+    }
+}
